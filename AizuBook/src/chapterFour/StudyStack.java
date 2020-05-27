2ac3313ff5de18de4982e7f8234b164de12514b1
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-05-27 23:57:02 +0900

    スタックの実装完了

--- AizuBook/src/chapterFour/StudyStack.java
+++ AizuBook/src/chapterFour/StudyStack.java
@@ -12,30 +12,26 @@ public class StudyStack {
 		String poland = sc.nextLine();
 
 		String[] e = poland.split(" ");
-		Stack<Integer> s = new Stack<>();
+		Stack<Integer> stack = new Stack<>();
 
 		for(int i = 0; i < e.length; i++) {
-			switch(e[i]) {
-			case "+":
-				s.push(s.pop() + s.pop());
-				break;
-
-			case "-":
-				s.push(-s.pop() + s.pop());
-				break;
-
-			case "*":
-				s.push(s.pop() * s.pop());
-				break;
-
-			default:
-				s.push(Integer.valueOf(e[i]));
-				break;
+			if(e[i].equals("+")) {
+				//1番目に取り出した数と2番目に取り出した数を足す
+				stack.push(stack.pop() + stack.pop());
+			}else if(e[i].equals("-")) {
+				//2番目に取り出した数から1番目に取り出したを引く
+				stack.push(-stack.pop() + stack.pop());
+			}else if(e[i].equals("*")) {
+				//1番目に取り出した数と2番目に取り出した数を掛ける
+				stack.push(stack.pop() * stack.pop());
+			}else {
+				//数字の場合は、スタックに数字を突っ込む
+				stack.push(Integer.parseInt(e[i]));
 			}
 		}
-		System.out.println(s.pop());
+
+		System.out.println(stack.pop());
 
 		sc.close();
 	}
-
 }
