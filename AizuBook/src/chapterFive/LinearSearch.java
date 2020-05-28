f76f837cab8144b510afe1b06569f03bc0579e70
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-05-28 23:37:15 +0900

    線形探索の実装完了

--- AizuBook/src/chapterFive/LinearSearch.java
+++ AizuBook/src/chapterFive/LinearSearch.java
@@ -1,36 +1,34 @@
 package chapterFive;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-
-
+//5-2 線形探索
 public class LinearSearch {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] s = new int[n];
+		int n = Integer.parseInt(sc.next());
+		ArrayList<Integer> S = new ArrayList<>();
 		for(int i = 0; i < n; i++) {
-			s[i] = sc.nextInt();
+			S.add(Integer.parseInt(sc.next()));
+		}
+		int q = Integer.parseInt(sc.next());
+		ArrayList<Integer> T = new ArrayList<>();
+		for(int i = 0; i < q; i++) {
+			T.add(Integer.parseInt(sc.next()));
 		}
 
-		int q = sc.nextInt();
-		int[] t = new int[n];
-		for(int i = 0; i < q; i++) {
-			t[i] = sc.nextInt();
-		}
 		sc.close();
+
 		//線形探索
 		int count = 0;
-		for(int i = 0; i < q; i++) {
-			for(int j = 0; j < n; j++) {
-				if(s[j] == t[i]) {
-					count++;
-					break;
-				}
+		for(int i = 0; i < T.size(); i++) {
+			int tmp = T.get(i);
+			if(S.contains(tmp)) {
+				count++;
 			}
 		}
 		System.out.println(count);
-		sc.close();
 	}
 }
